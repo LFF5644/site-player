fd956dd92b789fa941ec4ad94bc6a9b9ed252e8b
LFF5644 (LFF-Vivobook) <meerschweinchen5644@gmail.com>
2025-04-21 14:07:32 +0200

    fixed generator bug simple

--- server.service.js
+++ server.service.js
@@ -17,15 +17,25 @@ function simpleHash(str){
 	return hash;
 }
 async function* eventGenerator(client_id){
+	const l=a=>log("Generator-"+client_id+": "+a);
 	//const UPDATE_INTERVAL=5e3;
 	if(!svr.clients.has(client_id)) throw new Error("client not exist");
 	const client=svr.clients.get(client_id);
+	initial:{
+		const {
+			paused,
+			playing,
+			track,
+		}=player.playback;
+		yield ["log","CONNECTION-OPENED: "+client_id];
+		yield ["set-playback",JSON.stringify({playing,paused,track})];
+	};
 	while(svr.running){
 		const wait=client.wait();
-		yield ["log","still active connection!"];
+		yield ["log","NEXT-TICK: "+client_id];
 		while(client.requests.length>0){
 			const request=client.requests.pop();
-			log("request: "+request+", from client: "+client.id);
+			l("request: "+request);
 			if(request==="get_albums"){
 				const albums=(player.albums
 					.map(item=>({
@@ -39,21 +49,20 @@ async function* eventGenerator(client_id){
 				);
 				yield ["set-albums",JSON.stringify(albums)];
 			}
+			else if(request==="update_playback"){
+				const {
+					paused,
+					playing,
+					track,
+				}=player.playback;
+				yield ["log","playback changed."];
+				yield ["set-playback",JSON.stringify({playing,paused,track})];
+			}
 			else yield ["log","err unknown request in server side."];
 		}
 		// its the main-loop like in an game.
 		const info=await wait; // waits until the client needs data, false means exit generator.
 		if(info===false) break; // if info false break/stopping generator.
-		else if(!info) continue; // no custom action.
-		else if(info==="playback_change"){
-			const {
-				paused,
-				playing,
-				track,
-			}=player.playback;
-			yield ["log","playback changed."];
-			yield ["set-playback",JSON.stringify({playing,paused,track})];
-		}
 	}
 }
 
@@ -74,14 +83,15 @@ function removeClient(client){
 	svr.clients.delete(client.id);
 	svr.client_ids=svr.client_ids.filter(item=>item!==client.id);
 }
-function updateClientGenerator(info){
+function updateClientGenerator(request){
 	for(const id of svr.client_ids){
 		const client=svr.clients.get(id);
-		client.check(info); // stopping waiting and recheck for changes.
+		if(request) client.requests.push(request);
+		client.check(request===false?false:undefined); // stopping waiting and recheck for changes.
 	}
 }
 function onPlaybackChange(){
-	updateClientGenerator("playback_change");
+	updateClientGenerator("update_playback");
 }
 
 svr.eventGenerator=eventGenerator;
@@ -94,7 +104,7 @@ return async ()=>{
 	svr.running=false;
 	for(const id of svr.client_ids){
 		const client=svr.clients.get(id);
-		client.check(); // make the generator stop.
+		client.check(false); // make the generator stop.
 	}
 	svr.client_ids=[];
 	svr.clients.clear();
